a72ab239f371a53d612e0baf2f9ed0c585b3cf2e
Jonathan Miller <jonjmiller4991@gmail.com>
2014-12-06 22:41:35 -0500

    test4

--- teamParser.js
+++
@@ -1,8 +0,0 @@
-/*global module, SESSION, updateModule*/
-
-var pokemon = {name: "MissngNo.", number: "0", gen: "1", subgen: "0", gender: "0", ability: "1", nature: "0", lvl: "100", item: "0", forme: "0", shiny: "0", happiness: "0"};
-var team = new pokemon[6]();
-
-function checker(input) {
-    setOutput(team[1].name);
-}
